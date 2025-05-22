bcfcd53235777722e4756ee00a3a8c631cf349d3
EmanElattar26 <emanelattar26@gmail.com>
2025-05-22 12:17:27 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+    titleWeb: "أعتراف",
+    introTitle: 'هسألك سؤال ا',
+    introDesc: `جاوبي علية بنعم او لا`,
+    btnIntro: 'أضغطي هنا',
+    title: 'بصراحة 🥰',
+    desc: 'بتحبيني ',
+    btnYes: 'نعم',
+    btnNo: 'لا',
+    question: 'وانا كمان بموت فيكي <3',
+  
+}
